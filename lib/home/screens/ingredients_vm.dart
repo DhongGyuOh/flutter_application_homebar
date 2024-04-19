6646c1db45102e209039368ee8823c1bf3220020
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2024-04-19 14:52:48 +0900

    http post 추가

--- lib/home/screens/ingredients_vm.dart
+++ lib/home/screens/ingredients_vm.dart
@@ -18,15 +18,67 @@ class IngredientsViewModel extends AsyncNotifier<List<Ingredient>> {
     return ingList;
   }
 
+  ////GET////
+  //모든 재료 List 조회하기
   Future<List<Ingredient>> getIngredientList() async {
     Response response = await get(Uri.parse('$baseUrl/$getAllUrl'));
     if (response.statusCode == 200) {
-      print("200 OK");
+      print("getList 200 OK");
       final List ingredients = jsonDecode(utf8.decode(response.bodyBytes));
       //final List ingredients = jsonDecode(response.body);
       return ingredients.map((e) => Ingredient.fromJson(e)).toList();
     } else {
       throw Exception(response.reasonPhrase);
+    }
+  }
+
+  //id로 재료 조회하기
+  Future<Ingredient> getIngredient(String id) async {
+    Response response = await get(Uri.parse('$baseUrl/$id'));
+    if (response.statusCode == 200) {
+      print("getId 200 OK");
+      Ingredient ingredient = jsonDecode(utf8.decode(response.bodyBytes));
+      return ingredient;
+    } else {
+      throw Exception(response.reasonPhrase);
+    }
+  }
+
+  //모든 재료 갯수 가져오기
+  Future<int> getIngredientCount() async {
+    Response response = await get(Uri.parse('$baseUrl/count'));
+    if (response.statusCode == 200) {
+      print("getCount 200 OK");
+      int count = jsonDecode(utf8.decode(response.bodyBytes));
+      return count;
+    } else {
+      throw Exception(response.reasonPhrase);
+    }
+  }
+
+  ////POST////
+  //재료 추가하기, 수정하기
+  Future<dynamic> upsertIngredient(Ingredient ingredient) async {
+    final Map<String, dynamic> requestBody = ingredient.toJson();
+    Response response = await post(Uri.parse('$baseUrl/upsert'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(requestBody));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to upsert ingredient: ${response.statusCode}');
+    }
+  }
+
+  ////Delete////
+  Future<void> deleteIngredient(int id) async {
+    Response response = await delete(Uri.parse('$baseUrl/delete/$id'));
+    if (response.statusCode == 200) {
+      print('Ingredient with id $id deleted successfully.');
+    } else {
+      throw Exception('Failed to delete ingredient: ${response.statusCode}');
     }
   }
 }
